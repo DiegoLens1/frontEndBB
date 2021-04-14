af95eec60cd781801e7a72d7ca8cf3ec1cfe34b3
DiegoLens1 <72391105+DiegoLens1@users.noreply.github.com>
2021-04-14 23:24:46 +0200

    Add files via upload

---
+++ js/sound.js
@@ -0,0 +1,11 @@
+function soundClap(laugh){
+    document.getElementById("clap").play();
+}
+
+function soundLaugh(laugh){
+    document.getElementById("laugh").play();
+}
+
+function soundCry(laugh){
+    document.getElementById("cry").play();
+}
